95eeebe131d2237cd4d93d1e29af75b7d5394cd1
thihaeung <48879518+Morrowga@users.noreply.github.com>
2024-05-02 14:04:39 +0630

    Rename

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import "react-native-gesture-handler";
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
-import { StyleSheet, Text, View } from "react-native";
-import SplashScreen from './src/screens/SplashScreen';
-import Navigation from './src/navigators/Navigation';
+import Main from './src/main/Main';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigation />
+      <Main />
     </Provider>
   );
 }
