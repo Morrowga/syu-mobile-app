72c6bab39eb0f75eb1d1476a820f436203ddee52
david-dev-23711 <david@leaptechnology.com.sg>
2024-05-04 04:04:33 +0630

    add-logo

--- src/navigation/DrawerNavigator.jsx
+++ src/navigation/DrawerNavigator.jsx
@@ -1,7 +1,13 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  createDrawerNavigator,
+} from "@react-navigation/drawer";
+import { StyleSheet, Text, View, Image } from "react-native";
 import HomeScreen from "../screens/app/HomeScreen";
 import Icon from "react-native-vector-icons/Ionicons"; // Adjust based on your preferred icon set
+import { Box, Divider } from "native-base";
+
 const Drawer = createDrawerNavigator();
 
 const ProfileScreen = () => (
@@ -25,62 +31,89 @@ const AddToCartScreen = () => (
   </View>
 );
 
-const DrawerNavigator = () => (
-  <Drawer.Navigator
-    screenOptions={{
-      drawerActiveTintColor: "tomato",
-      drawerInactiveTintColor: "gray",
-      drawerLabelStyle: { fontSize: 15 },
-    }}
-  >
-    <Drawer.Screen
-      name="HomeDrawer"
-      component={HomeScreen}
-      options={{
-        drawerIcon: ({ color, size }) => (
-          <Icon name="home-outline" color={color} size={size} />
-        ),
-        drawerLabel: "Home",
-      }}
-    />
-    <Drawer.Screen
-      name="Profile"
-      component={ProfileScreen}
-      options={{
-        drawerIcon: ({ color, size }) => (
-          <Icon name="person-outline" color={color} size={size} />
-        ),
-      }}
-    />
-    <Drawer.Screen
-      name="OrderList"
-      component={OrderListScreen}
-      options={{
-        drawerIcon: ({ color, size }) => (
-          <Icon name="list-outline" color={color} size={size} />
-        ),
-      }}
-    />
-    <Drawer.Screen
-      name="Wishlist"
-      component={WishlistScreen}
-      options={{
-        drawerIcon: ({ color, size }) => (
-          <Icon name="heart-outline" color={color} size={size} />
-        ),
-      }}
-    />
-    <Drawer.Screen
-      name="AddToCart"
-      component={AddToCartScreen}
-      options={{
-        drawerIcon: ({ color, size }) => (
-          <Icon name="cart-outline" color={color} size={size} />
-        ),
-      }}
-    />
-  </Drawer.Navigator>
-);
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <View style={styles.drawerContent}>
+        <View>
+          <Box style={styles.logoBox}>
+            <Image
+              source={require("../../assets/logo3.png")}
+              style={{
+                width: 50,
+                height: 70,
+                resizeMode: "contain",
+                marginBottom: 20,
+              }}
+            />
+            <Text style={styles.logoText}>SYU MOBILE</Text>
+          </Box>
+        </View>
+      </View>
+      <Divider />
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={CustomDrawerContent}
+      screenOptions={({ route }) => ({
+        drawerActiveTintColor: "tomato",
+        drawerInactiveTintColor: "gray",
+        drawerLabelStyle: { fontSize: 15 },
+      })}
+    >
+      <Drawer.Screen
+        name="Dashboard"
+        component={HomeScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Icon name="home-outline" color={color} size={size} />
+          ),
+          drawerLabel: "Dashboard",
+        }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Icon name="person-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="OrderList"
+        component={OrderListScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Icon name="list-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Wishlist"
+        component={WishlistScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Icon name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="AddToCart"
+        component={AddToCartScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Icon name="cart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default DrawerNavigator;
 
@@ -89,5 +122,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  drawerContent: {
+    width: "auto",
+    height: 100,
+    paddingHorizontal: 10,
+  },
+  logoBox: { flexDirection: "row", alignItems: "center", paddingTop: 10 },
+  logoText: {
+    paddingBottom: 15,
+    paddingLeft: 10,
   },
 });
