b3599a0fcd4b76af293fe9c98d773a68433a00e1
david-dev-23711 <david@leaptechnology.com.sg>
2024-05-04 22:47:33 +0630

    add-otp-input

--- src/screens/guest/LoginScreen.jsx
+++ src/screens/guest/LoginScreen.jsx
@@ -1,38 +1,52 @@
 // Login.jsx
-import { View, Text,StyleSheet } from 'react-native';
-import { Button, Box } from "native-base";
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet } from "react-native";
+import { Button, Box, Input, Stack } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 const Login = () => {
   const navigation = useNavigation();
-  
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const sendOtp = () => {
+    navigation.navigate("Otp");
+  };
+
   return (
     <View style={styles.body}>
-        <Text style={styles.textStyle}>Login</Text>
-        <Box alignItems="center" style={styles.boxStyle}>
-          <Button onPress={() => navigation.navigate('Otp')}>OTP</Button>
-        </Box>
+      <Text style={styles.textStyle}>Login</Text>
+      <Stack space={4} w="75%" maxW="300px" mx="auto">
+        <Input
+          variant="rounded"
+          value={phoneNumber}
+          onChangeText={(e) => setPhoneNumber(e)}
+          keyboardType="phone-pad"
+          mt={4}
+          textAlign="center"
+        />
+      </Stack>
+
+      <Box alignItems="center" style={styles.boxStyle}>
+        <Button onPress={sendOtp}>SEND OTP</Button>
+      </Box>
     </View>
   );
 };
 
 export default Login;
 
-
 const styles = StyleSheet.create({
-    body: 
-    {
-        flex: 1,
-        backgroundColor: "#000",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    textStyle: 
-    {
-        color: '#fff',
-    },
-    boxStyle:
-    {
-      marginTop: 20
-    }
+  body: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    color: "#fff",
+    paddingBottom: 10,
+  },
+  boxStyle: {
+    marginTop: 20,
+  },
 });
--- src/screens/guest/OtpScreen.jsx
+++ src/screens/guest/OtpScreen.jsx
@@ -1,40 +1,53 @@
 // Otp.jsx
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 import { Button, Box } from "native-base";
 import { useDispatch, useSelector } from "react-redux";
-import { authSuccess } from '../../redux/slices/authSlice';
+import { authSuccess } from "../../redux/slices/authSlice";
+import OTPTextView from "react-native-otp-textinput";
+import { useRef } from "react";
 
 const OtpScreen = () => {
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-
+  let otpInput = useRef(null);
   return (
     <View style={styles.body}>
-        <Text style={styles.textStyle}>Login</Text>
-        <Box alignItems="center" style={styles.boxStyle}>
-          <Button onPress={() => dispatch(authSuccess())}>Reach Otp</Button>
-        </Box>
+      <Text style={styles.textStyle}>ENTER OTP CODE</Text>
+
+      <OTPTextView
+        containerStyle={styles.textInputContainer}
+        textInputStyle={styles.roundedTextInput}
+        inputCount={5}
+      />
+      <Box alignItems="center" style={styles.boxStyle}>
+        <Button onPress={() => dispatch(authSuccess())}>Reach Otp</Button>
+      </Box>
     </View>
   );
 };
 
 export default OtpScreen;
 
-
 const styles = StyleSheet.create({
-    body: 
-    {
-        flex: 1,
-        backgroundColor: "#000",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    textStyle: 
-    {
-        color: '#fff',
-    },
-    boxStyle:
-    {
-      marginTop: 20
-    }
+  body: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    color: "#fff",
+    paddingBottom: 10,
+  },
+  boxStyle: {
+    marginTop: 20,
+  },
+  textInputContainer: {
+    marginBottom: 20,
+  },
+
+  roundedTextInput: {
+    borderRadius: 10,
+    borderWidth: 4,
+  },
 });
