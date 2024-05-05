042ecf9a03c18a4fa5734036cb4d827bbc1b9c6d
thihaeung <48879518+Morrowga@users.noreply.github.com>
2024-05-06 00:43:13 +0630

    DetailModal warning dismiss

--- src/components/DetailModalBox.jsx
+++ src/components/DetailModalBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Modal, Button, Image } from "native-base";
 
-const DetailModalBox = ({ isOpen, onClose, imageSrc }) => {
+const DetailModalBox = ({ isOpen, onClose, imageSrc = 'https://i.pinimg.com/564x/d0/9e/99/d09e99ca67004c353063e7e4c20642c4.jpg' }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="lg">
       <Modal.Content maxWidth="400px">
