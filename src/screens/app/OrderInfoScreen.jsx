6b7fe517e11260cfaacc13b2b9852d69e0d8e6b3
thihaeung <48879518+Morrowga@users.noreply.github.com>
2024-05-06 20:12:21 +0630

    Order Info Page updated

--- src/screens/app/OrderInfoScreen.jsx
+++ src/screens/app/OrderInfoScreen.jsx
@@ -1,11 +1,27 @@
 import { StyleSheet, View,TouchableOpacity } from "react-native";
 import { useRoute } from '@react-navigation/native';
-import { Heading, Box,FlatList,Stack, Text, Badge,Divider,Button, VStack} from "native-base";
-import { useNavigation } from '@react-navigation/native';
+import { Heading, Box,FlatList,Stack, Text, Badge,Divider,Button,Image, VStack, HStack} from "native-base";
 import { useEffect } from "react";
+import { useNavigation } from '@react-navigation/native';
+
 
 const OrderInfoScreen = () => {
 
+  const orderStatus = true;
+  let buttonBottom;
+
+  if(orderStatus)
+  {
+    buttonBottom = <Button w="full" variant="outline" rounded="full">
+    Save Gallery
+    </Button> 
+  } else {
+    buttonBottom = <Button w="full" colorScheme="danger" variant="outline" rounded="full">
+     Payment Action Needed
+    </Button> 
+  }
+
+  
   const orderDetails = [
     { id: 1, name: 'Stickers', total_count: 2,total_amt: 4000 },
     { id: 2, name: 'Badges', total_count: 5, total_amt: 3000 },
@@ -55,7 +71,7 @@ const OrderInfoScreen = () => {
                 </Text>
               </Box>
               <Box>
-                <Text>5000 MMK</Text>
+                <Text>{item.total_amt} MMK</Text>
               </Box>
             </Stack>
           </Stack>
@@ -66,23 +82,49 @@ const OrderInfoScreen = () => {
 
       return (
         <View style={styles.container}>
-          <FlatList data={orderDetails} renderItem={renderItem} 
-             contentContainerStyle={{ padding: 10 }}
-             keyExtractor={item => item.id} 
-          />
-          <Box w={140}>
-            <Divider my="2" px={5} _light={{
-                bg: "gray.800"
-              }} _dark={{
-                bg: "gray.50"
-              }} />
+          <Box flex={1}>
+            <Box flexDirection="row" justifyContent="space-between" mx={5}>
+              <Image
+                source={require("../../../assets/innerlogo.png")}
+                alt="Logo Image"
+                style={{
+                  width: 90,
+                  height: 90,
+                  resizeMode: "contain",
+                }}
+              />
+              <Box my={5}>
+                  <Text fontSize={12}>Order No : Order#1234</Text>
+                  <Text fontSize={12}>Name : Thi Ha Aung</Text>
+              </Box>
+            </Box>
+            <Box>
+              <FlatList data={orderDetails} renderItem={renderItem} 
+                contentContainerStyle={{ padding: 10 }}
+                keyExtractor={item => item.id} 
+              />
+            </Box>
+            <Box display="grid" px={5} justifyContent="end" w="full">
+              <Divider my="2" px={5} _light={{ bg: "gray.800" }} _dark={{ bg: "gray.50" }} />
+            </Box>
+            <Box p={4} flexDirection="row" justifyContent="space-between">
+              <Heading size="sm" px="2.5">
+                  Total Count 
+              </Heading>
+              <Text>
+                  10000 MMK
+              </Text>
+            </Box>
           </Box>
+          <HStack m={5} justifyContent="flex-end">
+             {buttonBottom} 
+          </HStack>
         </View>
       )
 };
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-},
+    flex: 1
+  },
 });
 export default OrderInfoScreen;
