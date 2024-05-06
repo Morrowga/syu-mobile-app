9aff5bd0301a6cd6e0890a78fb0925f815df179b
thihaeung <48879518+Morrowga@users.noreply.github.com>
2024-05-06 22:31:40 +0630

    package conflict

--- src/screens/app/HomeScreen.jsx
+++ src/screens/app/HomeScreen.jsx
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 
-const HomeScreen = () => (
+const HomeScreen = () => {
+
+  const slides = [
+    { id: 1, image: require('../../../assets/innerlogo.png') },
+    { id: 2, image: require('../../../assets/innerlogo.png') },
+    { id: 3, image: require('../../../assets/innerlogo.png') },
+  ];
+  
+
   <View style={styles.container}>
     <Text>Home Screen</Text>
   </View>
-);
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
