0ac204c3d8c9dbdb3d4f31e1d22f9def07f56966
thihaeung <48879518+Morrowga@users.noreply.github.com>
2024-05-06 23:09:40 +0630

    Home UI Updated

--- src/screens/app/HomeScreen.jsx
+++ src/screens/app/HomeScreen.jsx
@@ -1,23 +1,98 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { 
+  AspectRatio, 
+  Image, 
+  Stack, 
+  Box, 
+  Heading, 
+  FlatList 
+} from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
 
-  const slides = [
-    { id: 1, image: require('../../../assets/innerlogo.png') },
-    { id: 2, image: require('../../../assets/innerlogo.png') },
-    { id: 3, image: require('../../../assets/innerlogo.png') },
-  ];
-  
+  const navigation = useNavigation();
 
-  <View style={styles.container}>
-    <Text>Home Screen</Text>
-  </View>
+  const data = [
+    {
+      id: 1,
+      name: "Feeds",
+      url: "https://i.pinimg.com/564x/62/25/11/6225112c610b1d75a888ed2ed9997a39.jpg",
+      route: 'Feeds'
+    },
+    {
+      id: 2,
+      name: "Customization",
+      url: "https://i.pinimg.com/736x/3e/b2/a1/3eb2a13096448488940afec4a2191e35.jpg",
+      route: 'Customization'
+    },
+  ];
+  const renderItem = ({ item }) => (
+    <Box>
+    <TouchableOpacity
+        onPress={() =>
+        navigation.navigate(item.route)
+      }
+    >
+      <Box
+        key={item}
+        flex={1}
+        width="100%"
+        mt={3}
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _dark={{
+          borderColor: "coolGray.600",
+          backgroundColor: "gray.700",
+        }}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+        _light={{
+          backgroundColor: "gray.50",
+        }}
+      >
+        <Box>
+          <AspectRatio w="100%" ratio={16 / 5}>
+            <Image source={{ uri: item.url }} alt="image" />
+          </AspectRatio>
+        </Box>
+        <Stack p="4" space={3}>
+          <Stack flexDirection="row" justifyContent="space-between" space={2}>
+            <Heading size="sm">{item.name}</Heading>
+          </Stack>
+        </Stack>
+      </Box>
+    </TouchableOpacity>
+    </Box>
+   
+  );
+
+  return (
+    <View style={styles.container}>
+      <Box alignItems="center">
+          <Image
+            source={require("../../../assets/innerlogo.png")}
+            alt="Logo Image"
+            style={{
+              width: 280,
+              height: 280,
+              resizeMode: "contain",
+            }}
+          />
+      </Box>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        contentContainerStyle={{ padding: 10 }}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-},
-});
+const styles = StyleSheet.create({});
+
 export default HomeScreen;
